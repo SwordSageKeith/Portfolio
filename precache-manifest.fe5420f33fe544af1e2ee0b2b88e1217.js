0f4bff49dc6e9ad97924a5f3d2a63d543629801b
Keith Prince <keithcprince@gmail.com>
2020-04-01 14:47:34 -0400

    Updates

---
+++ precache-manifest.fe5420f33fe544af1e2ee0b2b88e1217.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f069e0c9dd4b3100658e764d1e31d594",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "6e9da0de883598e12b4f",
+    "url": "/Portfolio/static/css/main.41bbea8e.chunk.css"
+  },
+  {
+    "revision": "2f5a533aa98aad51e25a",
+    "url": "/Portfolio/static/js/2.aa626b67.chunk.js"
+  },
+  {
+    "revision": "6e9da0de883598e12b4f",
+    "url": "/Portfolio/static/js/main.3076eee1.chunk.js"
+  },
+  {
+    "revision": "759367bd2688bb7ad12b",
+    "url": "/Portfolio/static/js/runtime-main.22d2c1c1.js"
+  },
+  {
+    "revision": "fe418dc86750f5ca94d60184f0bafb13",
+    "url": "/Portfolio/static/media/Family.fe418dc8.jpg"
+  },
+  {
+    "revision": "4de7ef40323d4511c689fdbbcfd7e942",
+    "url": "/Portfolio/static/media/Game.4de7ef40.PNG"
+  },
+  {
+    "revision": "ae67c22438a5efa9dd0092362e4f210b",
+    "url": "/Portfolio/static/media/Keith.ae67c224.png"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/Portfolio/static/media/Octocat.0ba2aa20.png"
+  },
+  {
+    "revision": "0bb072ba8ae85d8ca75d928a44c1d4ee",
+    "url": "/Portfolio/static/media/PokemonApp.0bb072ba.PNG"
+  },
+  {
+    "revision": "f278be7973516129af9e64d45a0c6f4d",
+    "url": "/Portfolio/static/media/c++.f278be79.png"
+  },
+  {
+    "revision": "e8460f0d8b9df01e9460bfd1ade0e8c8",
+    "url": "/Portfolio/static/media/cSharp.e8460f0d.png"
+  },
+  {
+    "revision": "92bca4df868aa72638c53a2cd916a0d7",
+    "url": "/Portfolio/static/media/discord.92bca4df.png"
+  },
+  {
+    "revision": "3090f898b40ce01f9db9ca6bccea74c2",
+    "url": "/Portfolio/static/media/dnd.3090f898.PNG"
+  },
+  {
+    "revision": "093cae237e05a3a06d41b11f52abc14c",
+    "url": "/Portfolio/static/media/goodmeal.093cae23.PNG"
+  },
+  {
+    "revision": "27e2f81f0b20c79a2b6dd093f06062d1",
+    "url": "/Portfolio/static/media/html-css-js-logos.27e2f81f.png"
+  },
+  {
+    "revision": "702d5ee48b184595693ac3c4f82c7d74",
+    "url": "/Portfolio/static/media/mail.702d5ee4.png"
+  },
+  {
+    "revision": "d8b9ecec37ead7d0d634cf7b77a1c6ca",
+    "url": "/Portfolio/static/media/unity.d8b9ecec.png"
+  },
+  {
+    "revision": "0111dbc669883b01fd7dc284aecf0117",
+    "url": "/Portfolio/static/media/zbrush-logo.0111dbc6.jpeg"
+  }
+]);
